3754280ba26347ffdb6ebb2afff4d01b09007e51
Lefteris Garyfalakis <lefterisgar@gmail.com>
2022-09-02 23:21:43 +0300

    Only print message if xclip is not installed

--- limoji.sh
+++ limoji.sh
@@ -11,10 +11,8 @@ if [ $XDG_SESSION_TYPE != "x11" ]; then
     echo not x11
 fi
 
-if hash xclip 2> /dev/null; then
+if ! hash xclip 2> /dev/null; then
     echo xclip installed
-else
-    echo xclip not installed
 fi
 
 echo ${!1} | tee >(copyToClipboard)
