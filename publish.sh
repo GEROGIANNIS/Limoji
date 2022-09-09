c061a10838a5e4e703e26e25d4803c0df05f3865
Lefteris Garyfalakis <lefterisgar@gmail.com>
2022-09-09 23:19:09 +0300

    Publish: A few improvements

--- publish.sh
+++ publish.sh
@@ -3,10 +3,23 @@
 export DEBFULLNAME="Lefteris Garyfalakis"
 export DEBEMAIL="lefterisgar@gmail.com"
 
+# Update changelog
 REL_VER=$(grep "^readonly VERSION" limoji | cut -d'"' -f2)
-dch -v "${REL_VER}" --distribution=unstable "New upstream release."
-git add limoji debian/changelog
-git commit -m "${REL_VER}"
-git tag "${REL_VER}"
-git push
-git push --tags
+dch -v "${REL_VER}" --distribution=unstable "New upstream release." > /dev/null
+
+echo "Creating the release commit..."
+
+# Commit the changes
+git add limoji debian/changelog > /dev/null
+git commit -m "${REL_VER}" > /dev/null
+
+echo "Tagging the new release..."
+
+# Tag the new release
+git tag "${REL_VER}" > /dev/null
+
+echo "Pushing tags..."
+
+# Push the release commit & tags
+git push > /dev/null
+git push --tags > /dev/null
